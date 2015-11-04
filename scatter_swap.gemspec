5109f7ca2131676b391f1b4909bdb661f82f6fc9
Rob Graham <altabyte@gmail.com>
2015-11-04 18:15:37 +0000

    Update RSpec tests to use the new `:expect` syntax.

--- scatter_swap.gemspec
+++ scatter_swap.gemspec
@@ -1,19 +1,31 @@
-# -*- encoding: utf-8 -*-
+# coding: utf-8
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'scatter_swap/version'
 
-Gem::Specification.new do |gem|
-  gem.name          = "scatter_swap"
-  gem.version       = ScatterSwap::VERSION
-  gem.authors       = ["Nathan Amick"]
-  gem.email         = ["github@nathanamick.com"]
-  gem.description   = %q{ScatterSwap is an integer hash function designed to have zero collisions, achieve avalanche, and be reversible.}
-  gem.summary       = %q{Minimal perfect hash function for 10 digit integers}
-  gem.homepage      = "https://github.com/namick/scatter_swap"
+Gem::Specification.new do |spec|
+  spec.name          = "scatter_swap"
+  spec.version       = ScatterSwap::VERSION
+  spec.authors       = ["Nathan Amick"]
+  spec.email         = ["github@nathanamick.com"]
+  spec.description   = %q{ScatterSwap is an integer hash function designed to have zero collisions, achieve avalanche, and be reversible.}
+  spec.summary       = %q{Minimal perfect hash function for 10 digit integers}
+  spec.homepage      = "https://github.com/namick/scatter_swap"
 
-  gem.files         = `git ls-files`.split($/)
-  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ["lib"]
+  # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
+  # delete this section to allow pushing this gem to any host.
+  if spec.respond_to?(:metadata)
+    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemerver.com'"
+  else
+    raise 'RubyGems 2.0 or newer is required to protect against public gem pushes.'
+  end
+
+  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.bindir        = 'exe'
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ['lib']
+
+  spec.add_development_dependency 'bundler', '~> 1.10'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec'
 end
--- spec/scatter_swap_spec.rb
+++ spec/scatter_swap_spec.rb
@@ -1,87 +1,98 @@
 require 'spec_helper'
 require 'scatter_swap'
 
-describe "#hash" do
-  it "should be 10 digits" do
-    100.times do |integer|
-      ScatterSwap.hash(integer).to_s.length.should == 10
+describe ScatterSwap do
+
+  it 'has a version number' do
+    expect(ScatterSwap::VERSION).not_to be nil
+  end
+
+  describe '#hash' do
+    it 'should be 10 digits' do
+      100.times do |integer|
+        expect(ScatterSwap.hash(integer).to_s.length).to eq 10
+      end
+    end
+
+    it 'should not be sequential' do
+      first = ScatterSwap.hash(1)
+      second = ScatterSwap.hash(2)
+      expect(second).not_to eq(first.to_i + 1)
+    end
+
+    it 'should be reversable' do
+      100.times do |integer|
+        hashed = ScatterSwap.hash(integer)
+        expect(ScatterSwap.reverse_hash(hashed).to_i).to eq(integer)
+      end
     end
   end
 
-  it "should not be sequential" do
-    first = ScatterSwap.hash(1)
-    second = ScatterSwap.hash(2)
-    second.should_not eql(first.to_i + 1)
+
+  describe '#swapper_map' do
+    before do
+      @map_set = []
+      s = ScatterSwap::Hasher.new(1)
+      10.times do |digit|
+        @map_set.push s.swapper_map(digit)
+      end
+    end
+
+    it 'should create a unique map array for each digit' do
+      expect(@map_set.length).to eq(10)
+      expect(@map_set.uniq.length).to eq(10)
+    end
+
+    it 'should include all 10 digits in each map' do
+      @map_set.each do |map|
+        expect(map.length).to eq(10)
+        expect(map.uniq.length).to eq(10)
+      end
+    end
   end
 
-  it "should be reversable" do
-    100.times do |integer|
-      hashed = ScatterSwap.hash(integer)
-      ScatterSwap.reverse_hash(hashed).to_i.should == integer
+
+  describe '#scatter' do
+    it 'should return a number different from original' do
+      100.times do |integer|
+        s = ScatterSwap::Hasher.new(integer)
+        original_array = s.working_array
+        s.scatter
+        #s.working_array.should_not == original_array
+        expect(s.working_array).not_to eq(original_array)
+      end
+    end
+
+    it 'should be reversable' do
+      100.times do |integer|
+        s = ScatterSwap::Hasher.new(integer)
+        original_array = s.working_array.clone
+        s.scatter
+        s.unscatter
+        expect(s.working_array).to eq(original_array)
+      end
+    end
+  end
+
+
+  describe '#swap' do
+    it 'should be different from original' do
+      100.times do |integer|
+        s = ScatterSwap::Hasher.new(integer)
+        original_array = s.working_array.clone
+        s.swap
+        expect(s.working_array).not_to eq(original_array)
+      end
+    end
+
+    it 'should be reversable' do
+      100.times do |integer|
+        s = ScatterSwap::Hasher.new(integer)
+        original_array = s.working_array.clone
+        s.swap
+        s.unswap
+        expect(s.working_array).to eq(original_array)
+      end
     end
   end
 end
-
-describe "#swapper_map" do
-  before do
-    @map_set = []
-    s = ScatterSwap::Hasher.new(1)
-    10.times do |digit|
-      @map_set.push s.swapper_map(digit)
-    end
-  end
-
-  it "should create a unique map array for each digit" do
-    @map_set.length.should == 10
-    @map_set.uniq.length.should == 10
-  end
-
-  it "should include all 10 digits in each map" do
-    @map_set.each do |map|
-      map.length.should == 10
-      map.uniq.length.should == 10
-    end
-  end
-end
-
-describe "#scatter" do
-  it "should return a number different from original" do
-    100.times do |integer|
-      s = ScatterSwap::Hasher.new(integer)
-      original_array = s.working_array
-      s.scatter
-      s.working_array.should_not == original_array
-    end
-  end
-
-  it "should be reversable" do
-    100.times do |integer|
-      s = ScatterSwap::Hasher.new(integer)
-      original_array = s.working_array.clone
-      s.scatter
-      s.unscatter
-      s.working_array.should == original_array
-    end
-  end
-end
-
-describe "#swap" do
-  it "should be different from original" do
-    100.times do |integer|
-      s = ScatterSwap::Hasher.new(integer)
-      original_array = s.working_array.clone
-      s.swap
-      s.working_array.should_not == original_array
-    end
-  end
-
-  it "should be reversable" do
-    100.times do |integer|
-      s = ScatterSwap::Hasher.new(integer)
-      original_array = s.working_array.clone
-      s.swap
-      s.unswap
-      s.working_array.should == original_array
-    end
-  end
-end  
